afab34437f1f40c2c5a0c01e910f8e8d751b3a47
iGitScor <sebastien.correaud@gmail.com>
2016-05-12 14:34:17 -0700

    Template with webpack init

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry:  './src',
+  output: {
+    path:     'builds',
+    filename: 'bundle.js',
+    publicPath: 'builds/',
+  },
+  module: {
+    loaders: [
+      {
+        test:   /\.js/,
+        loader: 'babel',
+        include: __dirname + '/src',
+      },
+      {
+        test:   /\.scss/,
+        loader: 'style!css!sass',
+      },
+    ],
+  },
+};
